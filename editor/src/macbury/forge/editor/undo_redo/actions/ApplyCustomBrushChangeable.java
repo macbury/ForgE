b64a9a73d7d52e86915303cc5b4771cf445a4e18
Arkadiusz Buras <macbury@gmail.com>
2015-03-18 12:36:38 +0100

    Reverse brush

--- editor/src/macbury/forge/editor/undo_redo/actions/ApplyCustomBrushChangeable.java
+++ editor/src/macbury/forge/editor/undo_redo/actions/ApplyCustomBrushChangeable.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Disposable;
 import macbury.forge.blocks.Block;
 import macbury.forge.editor.selection.AbstractSelection;
 import macbury.forge.utils.Vector3i;
+import macbury.forge.voxel.Voxel;
 import macbury.forge.voxel.VoxelMap;
 
 import javax.swing.*;
@@ -21,19 +24,43 @@ public class ApplyCustomBrushChangeable extends TerrainCursorChangeable {
   private static final String TAG = "ApplyCustomBrushChangeable";
   private final Pixmap pixmap;
   private final Block blockToDraw;
+
+  private Array<BlockSave> oldBlocks;
   private Vector3i start  = new Vector3i();
   private Vector3i end    = new Vector3i();
   private Vector3i cursor = new Vector3i();
+
   public ApplyCustomBrushChangeable(AbstractSelection selection, VoxelMap map, int scale, BrushType brushType, Block blockToDraw) {
     super(selection, map);
     this.pixmap      = brushType.getForScale(scale);
     this.blockToDraw = blockToDraw;
+    this.oldBlocks   = new Array<BlockSave>();
   }
 
   @Override
   public void revert() {
-
+    for(BlockSave save : oldBlocks) {
+      save.applyTo(map);
+    }
   }
+
+  private void saveBlock(Vector3i cursor) {
+    BlockSave save = new BlockSave(cursor, map.getVoxelForPosition(cursor));
+    if (!oldBlocks.contains(save, false)) {
+      oldBlocks.add(save);
+    }
+  }
+
+  private void putBlock(Block type, Vector3i pos) {
+    saveBlock(pos);
+    Voxel currentVoxel   = map.findOrInitializeVoxelForPosition(pos);
+    if (currentVoxel != null) {
+      currentVoxel.blockId = type.id;
+      currentVoxel.alginTo = alignToSide;
+      map.setVoxelForPosition(currentVoxel, pos);
+    }
+  }
+
 
   @Override
   public void apply() {
@@ -49,7 +76,7 @@ public class ApplyCustomBrushChangeable extends TerrainCursorChangeable {
 
         if (c >= 0) {
           cursor.set(x, from.y, z);
-          Gdx.app.log(TAG, cursor.toString());
+          //Gdx.app.log(TAG, cursor.toString());
           while(true) {
             if (map.isSolid(cursor.x, cursor.y-1, cursor.z) || map.isOutOfBounds(cursor.x, cursor.y-1, cursor.z)) {
               break;
@@ -57,9 +84,8 @@ public class ApplyCustomBrushChangeable extends TerrainCursorChangeable {
               cursor.y -= 1;
             }
 
-            //Gdx.app.log(TAG, cursor.toString());
           }
-          map.setBlockForPosition(blockToDraw, cursor);
+          putBlock(blockToDraw, cursor);
         }
       }
     }
@@ -90,9 +116,11 @@ public class ApplyCustomBrushChangeable extends TerrainCursorChangeable {
         int nh = Math.round(basePixmap.getHeight() * scale);
         int nw = Math.round(basePixmap.getWidth() * scale);
         Pixmap scaledPixmap = new Pixmap(nw, nh, Pixmap.Format.RGBA8888);
+        Pixmap.setFilter(Pixmap.Filter.BiLinear);
         scaledPixmap.setColor(Color.WHITE);
-        scaledPixmap.drawRectangle(0, 0, nw, nh);
-        scaledPixmap.drawPixmap(basePixmap, 0,0, scaledPixmap.getWidth(), scaledPixmap.getHeight(), 0, 0, basePixmap.getWidth(), basePixmap.getHeight());
+        scaledPixmap.fill();
+        scaledPixmap.drawPixmap(basePixmap, 0,0, basePixmap.getWidth(), basePixmap.getHeight(), 0, 0, scaledPixmap.getWidth(), scaledPixmap.getHeight());
+        //PixmapIO.writePNG(new FileHandle("/tmp/scaled.png"), scaledPixmap);
         //basePixmap.drawPixmap(scaledPixmap, 0,0, 0, 0, basePixmap.getWidth(), basePixmap.getHeight());
         caches.put(scale, scaledPixmap);
       }
