ed41b4f96524d3246620202698a27a0879c43496
Arkadiusz Buras <macbury@gmail.com>
2015-08-04 12:21:23 +0200

    Preparing geometry provider for terrain engine

---
+++ core/src/macbury/forge/terrain/geometry/DynamicGeometryProvider.java
@@ -0,0 +1,7 @@
+package macbury.forge.terrain.geometry;
+
+/**
+ * Created by macbury on 04.08.15.
+ */
+public class DynamicGeometryProvider {
+}
---
+++ core/src/macbury/forge/terrain/geometry/TerrainGeometryProvider.java
@@ -0,0 +1,9 @@
+package macbury.forge.terrain.geometry;
+
+import com.badlogic.gdx.utils.Disposable;
+
+/**
+ * Created by macbury on 04.08.15.
+ */
+public abstract class TerrainGeometryProvider implements Disposable {
+}
